3f92e0e69849f8614aa6f9a86f74daef63776389
Kajari <kajari@mapbox.com>
2016-11-18 18:21:41 -0500

    actually fixed Travis complaint

--- src/tools/components.cpp
+++ src/tools/components.cpp
@@ -50,11 +50,11 @@ void deleteFileIfExists(const std::string &file_name)
     }
 }
 
-std::size_t loadGraph(const char *path,
+std::size_t loadGraph(const std::string &path,
                       std::vector<extractor::QueryNode> &coordinate_list,
                       std::vector<TarjanEdge> &graph_edge_list)
 {
-    storage::io::FileReader file_reader<boost::filesystem::path>(path, storage::io::FileReader::VerifyFingerprint);
+    storage::io::FileReader file_reader(path, storage::io::FileReader::VerifyFingerprint);
 
     // load graph data
     std::vector<extractor::NodeBasedEdge> edge_list;
@@ -107,7 +107,7 @@ int main(int argc, char *argv[])
     }
 
     std::vector<osrm::tools::TarjanEdge> graph_edge_list;
-    auto number_of_nodes = osrm::tools::loadGraph(argv[1], coordinate_list, graph_edge_list);
+    auto number_of_nodes = osrm::tools::loadGraph(std::string(argv[1]), coordinate_list, graph_edge_list);
 
     tbb::parallel_sort(graph_edge_list.begin(), graph_edge_list.end());
     const auto graph = std::make_shared<osrm::tools::TarjanGraph>(number_of_nodes, graph_edge_list);
